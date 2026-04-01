7dbbcc902f7566968779360fc743bfe588ef9fff
Vishnu000866 <chaudharibishnu11@gmail.com>
2026-04-01 15:46:23 +0300

    display message on LCD

--- src/motor.java
+++ src/motor.java
@@ -24,5 +24,22 @@ public class motor {
         rightMotor.stop(false);
         Delay.msDelay(1000);
 
-    }
+        // do a 360 degree turn using rotate()
+        leftMotor.setspeed(250);
+        rightMotor.setspeed(250);
+
+        leftMotor.rotate(720, true); // ajust if needed for your robot 
+        rightMotor.rotate(-720);
+
+        //stop(true) = stop with braking 
+        leftMotor.stop(true);
+        rightMotor.stop(true);
+        Delay.msDelay(10000);
+
+        // Display message on LCD
+        LCD.clear();
+        LCD.drawString("Reached halfway,",0, 0);
+        LCD.drawString("returing to base",0, 0);
+        Delay.msDelay(3000);
+        
 }
