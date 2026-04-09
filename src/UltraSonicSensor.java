21e1489c48a35be9ab7c7cc9cfcd697042710a9f
Vishnu000866 <chaudharibishnu11@gmail.com>
2026-04-09 20:01:06 +0300

    imported the libraries and made a UltraSonic class

---
+++ src/UltraSonicSensor.java
@@ -0,0 +1,13 @@
+package src;
+
+import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.SampleProvider;
+import lejos.utility.Delay;
+
+ public class UltraSonicSensor {
+    }
