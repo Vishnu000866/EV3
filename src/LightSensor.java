d3fcdc429f59f3d8db46a073b549bedbc0dd0da6
Vishnu000866 <chaudharibishnu11@gmail.com>
2026-04-09 20:31:15 +0300

    basic light sensor reading

--- src/LightSensor.java
+++ src/LightSensor.java
@@ -11,6 +11,30 @@ import lejos.utility.Delay;
 
 public class LightSensor {
 
+     public static void main(String[] args) {
 
-    
+        EV3ColorSensor sensor = new EV3ColorSensor(SensorPort.S3);
+        SampleProvider lightMode = sensor.getRedMode();
+        float[] sample = new float[lightMode.sampleSize()];
+
+        LCD.clear();
+        LCD.drawString("Press any key", 0, 0);
+        Button.waitForAnyPress();
+
+        while (!Button.ESCAPE.isDown()) {
+
+            lightMode.fetchSample(sample, 0);
+            int lightValue = (int)(sample[0] * 100);
+
+            LCD.clear();
+            LCD.drawString("Light:", 0, 0);
+            LCD.drawString(lightValue + " %", 0, 1);
+
+            Delay.msDelay(100);
+        }
+
+        sensor.close();
+    }
+
+
 }
