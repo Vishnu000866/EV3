6853d6182e356dbcf70e591df134f4cdde403a03
Vishnu000866 <chaudharibishnu11@gmail.com>
2026-04-09 20:30:36 +0300

    added the important libraries for ev3 and also made a class LightSensor

--- package.bat
+++ package.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Java\jdk1.7.0_80\bin\jar" -cfe mug.jar motor -C src .
+"C:\Program Files\Java\jdk1.7.0_80\bin\jar" -cfe ultraaaa.jar UltraSonicSensor -C src .
---
+++ src/LightSensor.java
@@ -0,0 +1,16 @@
+package src;
+
+import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.robotics.SampleProvider;
+import lejos.utility.Delay;
+
+public class LightSensor {
+
+
+    
+}
