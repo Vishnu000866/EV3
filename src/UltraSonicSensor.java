56ad7f6634cce01af6bc35f7b77a64e70657feb9
Vishnu000866 <chaudharibishnu11@gmail.com>
2026-04-09 20:06:21 +0300

    added basic ultrasonic sensor movement

--- src/UltraSonicSensor.java
+++ src/UltraSonicSensor.java
@@ -10,4 +10,42 @@ import lejos.robotics.SampleProvider;
 import lejos.utility.Delay;
 
  public class UltraSonicSensor {
+    public static void main(String[] args) {
+
+        EV3UltrasonicSensor ultrasonicSensor = new EV3UltrasonicSensor(SensorPort.S2);
+        SampleProvider distanceMode = ultrasonicSensor.getDistanceMode();
+        float[] sample = new float[distanceMode.sampleSize()];
+
+        EV3LargeRegulatedMotor motorA = new EV3LargeRegulatedMotor(MotorPort.A);
+        EV3LargeRegulatedMotor motorB = new EV3LargeRegulatedMotor(MotorPort.B);
+
+        int speed = 250;
+
+        LCD.clear();
+        LCD.drawString("Press any key", 0, 0);
+        Button.waitForAnyPress();
+
+        motorA.setSpeed(speed);
+        motorB.setSpeed(speed);
+        motorA.forward();
+        motorB.forward();
+
+        while (!Button.ESCAPE.isDown()) {
+            distanceMode.fetchSample(sample, 0);
+            float distance = sample[0];
+
+            LCD.clear();
+            LCD.drawString("Distance:", 0, 0);
+            LCD.drawString((distance * 100) + " cm", 0, 1);
+
+            Delay.msDelay(100);
+        }
+
+        motorA.stop(true);
+        motorB.stop();
+
+        ultrasonicSensor.close();
+        motorA.close();
+        motorB.close();
+    }
     }
