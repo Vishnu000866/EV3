5d6cae1674062c40c134171767474aa6824adde9
Vishnu000866 <chaudharibishnu11@gmail.com>
2026-04-28 16:20:52 +0300

    light threshold is 0.35

--- src/multisensor/LightSensor.java
+++ src/multisensor/LightSensor.java
@@ -10,7 +10,21 @@ import lejos.utility.Delay;
 public class LightSensor {
     private static EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S1);
     private static SensorMode lightMode = colorSensor.getRedMode();
+    private static float[] sample = new float[lightMode.sampleSize()];
+    private static float lightThreshold = 0.35f;
+    public static void calibrate() {
 
+
+
+
+
+
+
+
+
+
+        
+    }
 
 
 
